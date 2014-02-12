13d8cf0a63355f8df07afd717c7a51a1f40d00be
Chancelor A. Fitzgerald <pinbal52@gmail.com>
2014-02-12 03:48:55 -0500

    Completed Exercise 1

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,12 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+car = "trailblazer"
+template = ERB.new "My model of my car is: <%= car %>"
+puts template.result(binding)
+
+colors = ["red","blue","yellow"]
+template = ERB.new "<% colors.each do |color| %> One primary color is <%= color %>
+ <% end %> "
+puts template.result(binding) 
